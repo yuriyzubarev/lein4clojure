bae3af17421d65062d2d4e5d71361276c87454b5
Yuriy Zubarev <yzubarev@sphere.local>
2012-10-21 21:14:23 -0700

    tiny progress

--- src/leiningen/lein4clojure.clj
+++ src/leiningen/lein4clojure.clj
@@ -1,9 +1,28 @@
 (ns leiningen.lein4clojure
 	(:use 
-        [clojure.data.json]))
+        [clojure.data.json]
+        [clojure.string :as str]))
+
+(import '(java.io File))
+
+(defn- fetch-exercise [n]
+	(read-json (slurp (str n ".json"))))
+	; (println (slurp (str "http://4clojure.com/api/problem/" n))))
+
+(defn- excercise-file-name [n jdoc]
+	(str (format "%03d" n) "-" (replace (lower-case (:title jdoc)) " " "-") ".clj"))
+
+(defn- exercise-exist? [fname]
+	(let [f (File. fname)]
+		(.exists f)))
 
 (defn lein4clojure
   "I don't do a lot."
   [project & args]
-  (println "Hi!" (slurp "http://4clojure.com/api/problem/1")))
+  (do 
+  	(println "Hi!") 
+  	(println (fetch-exercise 1))
+  	(println (excercise-file-name 1 (fetch-exercise 1)))
+  	(println (exercise-exist? (excercise-file-name 1 (fetch-exercise 1))))))
+
 
