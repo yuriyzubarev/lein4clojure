e304425edaee2ba265d0f53380bf7e1ac1ca3a8f
Yuriy Zubarev <yzubarev@sphere.kokanee.abebooks.com>
2012-12-24 13:14:57 -0800

    getting to the point where generated test is ready for manual modification (only tested for the first 4clojure exercise)

--- src/leiningen/lein4clojure.clj
+++ src/leiningen/lein4clojure.clj
@@ -1,17 +1,20 @@
 (ns leiningen.lein4clojure
-	(:use 
-        [clojure.data.json]
+	(:require 
+        [clojure.data.json :as json]
         [clojure.string :as str]
-        [clostache.parser]))
+        [clostache.parser :as clostache]))
 
 (import '(java.io File))
 
 (defn- fetch-exercise [n]
-	(read-json (slurp (str n ".json"))))
+	(json/read-json (slurp (str n ".json"))))
 	; (println (slurp (str "http://4clojure.com/api/problem/" n))))
 
+(defn- excercise-name [jdoc]
+  (str/replace (str/lower-case (:title jdoc)) " " "-"))
+
 (defn- excercise-ns [n jdoc]
-  (str (format "%03d" n) "_" (replace (lower-case (:title jdoc)) " " "_")))
+  (str (format "%03d" n) "_" (str/replace (str/lower-case (:title jdoc)) " " "_")))
 
 (defn- excercise-file-name [n jdoc]
 	(str "test/leiningen/" (excercise-ns n jdoc) ".clj"))
@@ -21,7 +24,10 @@
 		(.exists f)))
 
 (defn- create-exercise [fname jdoc]
-	(spit fname (render-resource "resources/hello.mustache" jdoc)))
+	(spit fname (clostache/render-resource "resources/hello.mustache" jdoc)))
+
+(defn- reformat [s fname]
+  (str/replace s "__" (str "(" fname ")")))
 
 (defn lein4clojure
   "I don't do a lot."
@@ -30,15 +36,21 @@
   	(println "Hi!") 
   	(let [
         jdoc (fetch-exercise 1)
+        test-name (excercise-name jdoc)
         test-file-name (excercise-file-name 1 jdoc)
-        test-file-ns (excercise-ns 1 jdoc)]
+        test-file-ns (excercise-ns 1 jdoc)
+        reformat-with-name #(reformat % test-name)]
 	  	(println jdoc)
   		(if (exercise-exist? test-file-name)
   			(do
   				(println "Exist"))
   			(do
   				(println "Doesn't exist")
+          (println (assoc jdoc :lein4clojure-ns test-file-ns))
   				(create-exercise 
-  					test-file-name (assoc jdoc :lein4clojure-ns test-file-ns)))))))
+  					test-file-name (assoc jdoc 
+              :l4c-ns test-file-ns 
+              :l4c-tests (map reformat-with-name (:tests jdoc))
+              :l4c-test-name test-name)))))))
 
 
